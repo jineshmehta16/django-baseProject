73b3b460db1019ce92bc1ee4986040ea7c7dc53b
Jinesh Mehta <mehtajinesh11@gmail.com>
2018-12-30 00:33:58 +0530

    - user model updated

--- webservice/app/admin.py
+++ webservice/app/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 
 from .models import Users
-
-class UserAdmin(admin.ModelAdmin):
-    admin.site.register(Users)
+admin.site.register(Users)
--- webservice/app/models.py
+++ webservice/app/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Users(models.Model):
     firstName = models.CharField(max_length=20)
-    lastName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=30)
+    emailId = models.CharField(max_length=12)
+    contactNumber = models.IntegerField()
+    address = models.CharField(max_length=100)
+    userId = models.IntegerField()
 
     def __str__(self):
         return self.firstName
