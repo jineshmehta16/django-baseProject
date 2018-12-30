63f788389fea861911a4c49946082122eea16c26
Jinesh Mehta <mehtajinesh11@gmail.com>
2018-12-31 01:26:07 +0530

    - Roles and User basic table

--- webservice/app/admin.py
+++ webservice/app/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 
 from .models import Users
-admin.site.register(Users)
+from .models import Roles
+
+
+@admin.register(Users)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['firstName', 'lastName', 'id', 'userId', 'emailId', 'contactNumber']
+
+
+@admin.register(Roles)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['roleId', 'roleName']
+
+
+
+
+
